0055451b9d132036686a6822e71de44e3fed0b61
lauderdaleerin <lauderdale.erin@gmail.com>
2024-01-31 13:12:45 -0800

    Working new nav bar
    Sticky bug on desktop response

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
 `use strict`;
-const navBoxCloseEl = document.querySelector(`.nav-box`);
-const navBoxOpenEl = document.querySelector(`.nav-box-open`);
-const openButton = document.querySelector(`.nav-button-open`);
-const closeButton = document.querySelector(`.nav-button-close`);
-const overlay = document.querySelector(`.overlay`);
-
-//Open navigation
-const openNav = function () {
-  navBoxOpenEl.classList.remove(`hidden`);
-  overlay.classList.remove(`hidden`);
-};
-
-navBoxCloseEl.addEventListener(`click`, openNav);
-
-//Close navigation
-const closeNav = function () {
-  navBoxOpenEl.classList.add(`hidden`);
-  overlay.classList.add(`hidden`);
-};
-closeButton.addEventListener(`click`, closeNav);
-
-document.addEventListener(`keydown`, function (e) {
-  if (e.key === `Escape` && !navBoxOpenEl.classList.contains(`hidden`))
-    closeNav();
-});
+function show() {
+  document.getElementById(`navigation`).classList.toggle(`active`);
+  document.querySelector(`.overlay`).classList.toggle(`hidden`);
+}
