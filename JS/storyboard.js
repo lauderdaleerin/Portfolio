55acc04d163ea2921abaf9d145d144cc006f4e1b
lauderdaleerin <lauderdale.erin@gmail.com>
2024-03-20 17:09:41 -0700

    More working version of storyboard slider on test page

--- JS/storyboard.js
+++ JS/storyboard.js
@@ -9,22 +9,49 @@ const dots = document.querySelector(`.dots`);
 let curSlide = 0;
 const maxSlides = slides.length - 1;
 
+// const slideHeight = function () {
+//   let maxH = 0;
+//   slides.forEach(s => {
+//     if (s.height >= maxH) {
+//       maxH = s.height;
+//     } else return;
+//   });
+//   storyboard.style.height = `${maxH}px`;
+// };
+
 const createDots = function () {
   slides.forEach((_, i) => {
     dots.insertAdjacentHTML(
       `beforeend`,
       `<button class="dots-dot" data-slide="${i}"></button>`
     );
-    console.log(`dot`);
   });
 };
-createDots();
+
+const activeDot = function (slide) {
+  document
+    .querySelectorAll(`.dots-dot`)
+    .forEach(dot => dot.classList.remove(`dots-dot-active`));
+
+  document
+    .querySelector(`.dots-dot[data-slide="${slide}"]`)
+    .classList.add(`dots-dot-active`);
+};
 
 const slidesFunc = function (slide) {
   slides.forEach(
     (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
   );
 };
+
+const init = function () {
+  createDots();
+  slidesFunc(curSlide);
+  activeDot(curSlide);
+  // slideHeight();
+};
+
+init();
 
 const nextSlide = function () {
   if (curSlide === maxSlides) {
@@ -33,6 +60,7 @@ const nextSlide = function () {
     curSlide++;
   }
   slidesFunc(curSlide);
+  activeDot(curSlide);
 };
 
 const prevSlide = function () {
@@ -42,6 +70,7 @@ const prevSlide = function () {
     curSlide--;
   }
   slidesFunc(curSlide);
+  activeDot(curSlide);
 };
 
 btnRight.addEventListener(`click`, nextSlide);
@@ -49,4 +78,12 @@ btnLeft.addEventListener(`click`, prevSlide);
 document.addEventListener(`keydown`, function (e) {
   if (e.key === `ArrowLeft`) prevSlide();
   if (e.key === `ArrowRight`) nextSlide();
+});
+
+dots.addEventListener(`click`, function (e) {
+  if (e.target.classList.contains(`dots-dot`)) {
+    const { slide } = e.target.dataset;
+    slidesFunc(slide);
+    activeDot(slide);
+  }
 });
