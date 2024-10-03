afe5a19c45b0548c34876b5e6c1cde640fc3f2bb
lauderdaleerin <lauderdale.erin@gmail.com>
2024-10-03 14:29:14 -0700

    Mobile fixes
    about ordering, img max width, mobile menu icons

--- JS/newmenu.js
+++ JS/newmenu.js
@@ -18,7 +18,7 @@ const menuIcon = document.querySelector(`.menu-icon`);
 
 mobileBtn.addEventListener(`click`, function () {
   if (mobileMenu.style.display === `none`) {
-    menuIcon.src = `Portfolio/images/icons/x.svg`;
+    menuIcon.src = `https://raw.githubusercontent.com/lauderdaleerin/Portfolio/4166b9cb91becac95a12af1fc40ba7621514c3f6/images/icons/x.svg`;
 
     mobileBtn.style.backgroundColor = `#00bdaa`;
     mobileBtn.style.boxShadow = `inset .25rem .25rem rgba(0, 0, 0, 0.35)`;
@@ -27,7 +27,7 @@ mobileBtn.addEventListener(`click`, function () {
       item.style.height = `2.5em`;
     });
   } else {
-    menuIcon.src = `/Portfolio/images/new/icons/tridot.svg`;
+    menuIcon.src = `https://raw.githubusercontent.com/lauderdaleerin/Portfolio/4166b9cb91becac95a12af1fc40ba7621514c3f6/images/icons/tridot.svg`;
     mobileBtn.style.backgroundColor = `#a1dd70`;
     mobileBtn.style.boxShadow = `0.25rem 0.25rem rgba(0, 0, 0, 0.35)`;
     mobileMenu.style.display = `none`;
