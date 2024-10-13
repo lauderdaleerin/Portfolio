b10e237d6023e2b826e6d68ef080e5958104d3c0
lauderdaleerin <lauderdale.erin@gmail.com>
2024-10-13 11:32:59 -0700

    Removed bubble animation, updated button styling

--- JS/bubbles.js
+++ JS/bubbles.js
@@ -4,8 +4,8 @@ const label = document.querySelectorAll(`.hidden-txt`);
 
 skillItem.forEach(function (item, i) {
   item.addEventListener(`mouseover`, function () {
-    label[i].style.display = `flex`;
-    label[i].style.transform = `translateX(0%)`;
+    label[i].style.display = `block`;
+    label[i].style.transform = `translateX(10%)`;
     bubbles[i].style.boxShadow = `inset 0.25rem 0.25rem rgba(0, 0, 0, 0.35)`;
   });
   item.addEventListener(`mouseout`, function () {
